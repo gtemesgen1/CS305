208e7471dfa4682bbe63eba08a25294648525ab7
Girma Temesgen <girtem123@gmail.com>
2021-08-31 21:16:25 -0500

    commit

---
+++ W1D2/Salescom1.js
@@ -0,0 +1,25 @@
+function computeSalesCommission(num){
+    /*Defining Table
+    input 
+     salesMan
+     persent of commission
+
+
+     process output
+
+    */
+   let sales = 0;
+    let salesAmount= 0;
+    let salesMan = prompt("are you paid salary ?");{
+        if(salesMan === "salaried"){
+            if(sales<300){
+                console.log("no commission");
+            }
+
+        }
+
+    }
+    
+
+}
+computeSalesCommission(250);
